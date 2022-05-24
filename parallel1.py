8f99171fcbe547d8b59bf53d1482eb76bbb4654c
Pham Hoang Phuong <phuong-pham@cybozu.vn>
2022-05-24 08:32:41 +0700

    add convolution kernel

--- parallel1.py
+++ parallel1.py
@@ -57,12 +57,8 @@ def ConvolutionKernel(input_image, output_image, filters, bias, stride):
       while column + filter_size <= img_w:
         # output_image[0, r, c, n_f] = np.sum(filters[n_f] * input_image[0, row:row + filters.shape[1], column:column + filters.shape[2],:]) + bias[n_f]
         for channel in range(0, n_c):
-          for filterR in range(0, filter_size):
-            for filterC in range(0, filter_size):
-              inR = int((row-filter_size/2)+filterR)
-              inC = int((column-filter_size/2)+filterC)
-              inR = min(img_h-1 , max(0, inR))
-              inC = min(img_w-1 , max(0, inC))
+          for filterR, inR in zip(range(0, filter_size), range(row,row+filter_size)):
+            for filterC,inC in zip(range(0, filter_size), range(column,column+filter_size)):
               output_image[0, r, c, n_f] += filters[n_f,filterR,filterC,channel] * input_image[0, inR, inC, channel]
         output_image[0, r, c, n_f] += bias[n_f]
         column+= stride
@@ -92,25 +88,6 @@ def convolution(image, Filter, bias, stride=1):
 
     out = np.zeros((1, out_h, out_w, n_f))
 
-    # convolution of image_array with filter yeilds out_array
-    # for i in range of no.of filters
-    # define a row , out_y variabless to hover along rows of image, out_matrix respectively
-    # define a column , out_x variables to hover along columns of image, out_matrix respectively
-    # convolution is done in the ranges of image_height to image_width
-    # for i in range(n_f):
-    #     row = out_row = 0
-
-    #     while row + f <= img_h:
-
-    #         column = out_column = 0
-
-    #         while column + f <= img_w:
-    #             out[0, out_row, out_column, i] = np.sum(Filter[i] * image[0, row: row + f, column: column + f,:]) + bias[i]
-    #             column += stride
-    #             out_column += 1
-
-    #         row += stride
-    #         out_row += 1
     block_size = (32,32)
     grid_size = (math.ceil(out.shape[2]/block_size[0]),
                 math.ceil(out.shape[1]/block_size[1]),
@@ -123,60 +100,55 @@ def convolution(image, Filter, bias, stride=1):
 
     return out
 
-@jit
-def convolution2(image, Filter, bias, stride=1):
-    # convolution of input image with a filter of dimensions(n_f,n_c,f,f)
-    # n_f is number filters
-    # n_fc is number channels
-    # f,f are height & width
+# @jit
+# def convolution2(image, Filter, bias, stride=1):
+#     # convolution of input image with a filter of dimensions(n_f,n_c,f,f)
+#     # n_f is number filters
+#     # n_fc is number channels
+#     # f,f are height & width
 
-    # image dimensions(n_c, image_h, image_w)
-    # n_c is number channels in image
-    # img_h is height of image
-    # img_w is width of image
+#     # image dimensions(n_c, image_h, image_w)
+#     # n_c is number channels in image
+#     # img_h is height of image
+#     # img_w is width of image
 
-    (_, img_h, img_w, n_c) = image.shape
-    (n_f, f, f, n_fc) = Filter.shape
+#     (_, img_h, img_w, n_c) = image.shape
+#     (n_f, f, f, n_fc) = Filter.shape
 
-    # output dimensions after convolution
-    out_h = int((img_h - f) / stride) + 1  # height of output matrix
-    out_w = int((img_h - f) / stride) + 1  # width of output matrix
-    # n_f will be the depth of the matrix
+#     # output dimensions after convolution
+#     out_h = int((img_h - f) / stride) + 1  # height of output matrix
+#     out_w = int((img_h - f) / stride) + 1  # width of output matrix
+#     # n_f will be the depth of the matrix
 
-    out = np.zeros((1, out_h, out_w, n_f))
+#     out = np.zeros((1, out_h, out_w, n_f))
 
-    # convolution of image_array with filter yeilds out_array
-    # for i in range of no.of filters
-    # define a row , out_y variabless to hover along rows of image, out_matrix respectively
-    # define a column , out_x variables to hover along columns of image, out_matrix respectively
-    # convolution is done in the ranges of image_height to image_width
-    for i in range(n_f):
-        row = out_row = 0
+#     # convolution of image_array with filter yeilds out_array
+#     # for i in range of no.of filters
+#     # define a row , out_y variabless to hover along rows of image, out_matrix respectively
+#     # define a column , out_x variables to hover along columns of image, out_matrix respectively
+#     # convolution is done in the ranges of image_height to image_width
+#     for i in range(n_f):
+#         row = out_row = 0
+#         while row + f <= img_h:
+#             column = out_column = 0
 
-        while row + f <= img_h:
+#             while column + f <= img_w:
+#                 for channel in range(0, n_c):
+#                   for filterR, inR in zip(range(0, f), range(row,row+f)):
+#                     for filterC, inC in zip(range(0, f), range(column,column+f)):
+#                       out[0, out_row, out_column, i] += Filter[i,filterR,filterC,channel] * image[0, inR, inC, channel]
+#                 out[0, out_row, out_column, i] += bias[i]
+#                 column += stride
+#                 out_column += 1
 
-            column = out_column = 0
+#             row += stride
+#             out_row += 1
+        
+#         print(i)
 
-            while column + f <= img_w:
-                # out[0, out_row, out_column, i] = np.sum(Filter[i] * image[0, row: row + f, column: column + f,:]) + bias[i]
-                for channel in range(0, n_c):
-                  for filterR in range(0, f):
-                    for filterC in range(0, f):
-                      inR = int((row-f/2)+filterR)
-                      inC = int((column-f/2)+filterC)
-                      inR = min(img_h-1 , max(0, inR))
-                      inC = min(img_w-1 , max(0, inC))
-                      out[0, out_row, out_column, i] += Filter[i,filterR,filterC,channel] * image[0, inR, inC, channel]
-                out[0, out_row, out_column, i] += bias[i]
-                column += stride
-                out_column += 1
+#     print(out.shape)
 
-            row += stride
-            out_row += 1
-
-    print("ok chua", out.shape)
-
-    return out
+#     return out
 
 
 tic = time.perf_counter()
